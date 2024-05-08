67b828e40d7eee592e8966f0a59f7038d6c30f57
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-05-08 09:56:04 +0500

    Add files via upload

---
+++ 17-Arrays-Rem-Insert_Ext-Elem/app.js
@@ -0,0 +1,26 @@
+
+var pets =["Dog"," Cat", "Bird"];
+
+pets.shift();
+console.log(pets);
+
+var pets =["Dog"," Cat", "Bird"];
+
+pets.unshift("Cow");
+console.log(pets);
+
+var bird = ["Pig","Duck","Emu"];
+
+bird.splice( 0,2,"murad","haris")
+
+console.log(bird);
+
+var students = ["janzaib", "naseebkhan","fazal"," Murad","Hidayat"];
+
+ var copyarr = students.slice(1,4);
+ console.log(copyarr)
+
+ var a = ["Rehman","Arsalan","Majid"];
+
+ a.splice  ( 1,0,"Riyaz")
+console.log(a)
